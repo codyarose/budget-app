386c2ca3154e1b31fd7f20a3d15d443b7e752f15
codyarose <crose992@gmail.com>
2020-09-01 00:15:21 -0500

    Removed unnecessary action tests and added reducer tests for expensesSlice

--- src/features/expenses/expensesSlice.test.ts
+++ src/features/expenses/expensesSlice.test.ts
@@ -1,107 +1,112 @@
+jest.mock('uuid')
+import { v4 } from 'uuid'
+
+const mockedUuid = v4 as jest.Mocked<typeof v4>
+
 import expenses, {
 	initialState,
 	addExpense,
 	editExpense,
 	removeExpense,
+	selectExpenses,
 } from './expensesSlice'
+import { initialState as initialFilterState } from '../filter/filterSlice'
 
 describe('expenses reducer', () => {
-	it('should handle initial state', () => {
-		const nextState = initialState
-
-		const result = expenses(undefined, { type: undefined })
-
-		expect(result).toEqual(nextState)
-	})
-
-	it('should create action to add expense with provided values', () => {
-		const expenseData = {
-			description: 'Rent',
-			note: 'This is a note',
-			amount: 147500,
+	const mockData = [
+		{
+			id: '123abc',
+			description: 'Title',
+			note: '',
+			amount: 140500,
 			createdAt: 1598763926001,
+		},
+		{
+			id: '456def',
+			description: 'Blah',
+			note: 'This is a note',
+			amount: 2650,
+			createdAt: 1598763926009,
+		},
+		{
+			id: '789ghi',
+			description: 'Another one',
+			note: 'Text goes here',
+			amount: 340098,
+			createdAt: 1598763926345,
+		},
+	]
+
+	it('should handle initial state', () => {
+		const nextState = expenses(undefined, { type: '@@INIT' })
+
+		const result = initialState
+
+		expect(nextState).toEqual(result)
+	})
+
+	it('should remove expense by id', () => {
+		const nextState = expenses(mockData, removeExpense(mockData[1].id))
+
+		const rootState = { expenses: nextState, filter: initialFilterState }
+		expect(selectExpenses(rootState)).toEqual([mockData[0], mockData[2]])
+	})
+
+	it('should not remove expense if id not found', () => {
+		const nextState = expenses(mockData, removeExpense('-1'))
+
+		const rootState = { expenses: nextState, filter: initialFilterState }
+		expect(selectExpenses(rootState)).toEqual(mockData)
+	})
+
+	it('should add expense', () => {
+		const data = {
+			id: mockedUuid(),
+			description: 'New expense',
+			note: 'a note',
+			amount: 99930,
+			createdAt: 1598763926666,
 		}
-		expect(addExpense(expenseData)).toEqual({
-			type: addExpense.type,
-			payload: { ...expenseData },
-		})
+		const nextState = expenses(mockData, addExpense(data))
+
+		const rootState = { expenses: nextState, filter: initialFilterState }
+		expect(selectExpenses(rootState)).toEqual([...mockData, data])
 	})
 
-	it('should create action to add expense with default values', () => {
-		const defaultData = {
-			description: '',
-			note: '',
-			amount: 0,
-			createdAt: 0,
-		}
-
-		expect(addExpense(defaultData)).toEqual({
-			type: addExpense.type,
-			payload: { ...defaultData },
-		})
-	})
-
-	it('should create action to remove expense', () => {
-		expect(removeExpense('123abc')).toEqual({
-			type: removeExpense.type,
-			payload: '123abc',
-		})
-	})
-
-	it('should create action to edit expense', () => {
-		const expenseData = {
-			description: 'New description',
-			note: '',
-			amount: 123456,
-			createdAt: 20383787,
-		}
-
-		expect(
-			editExpense({
-				id: '123abc',
-				editedExpense: expenseData,
-			}),
-		).toEqual({
-			type: editExpense.type,
-			payload: {
-				id: '123abc',
-				editedExpense: { ...expenseData },
-			},
-		})
-	})
-
-	it('should handle removeExpense', () => {
-		expect(
-			expenses(
-				[
-					{
-						id: '123abc',
-						description: 'Title',
-						note: '',
-						amount: 140500,
-						createdAt: 1598763926001,
-					},
-					{
-						id: '456def',
-						description: 'Blah',
-						note: 'This is a note',
-						amount: 2650,
-						createdAt: 1598763926009,
-					},
-				],
-				{
-					type: removeExpense.type,
-					payload: '123abc',
-				},
-			),
-		).toEqual([
-			{
-				id: '456def',
-				description: 'Blah',
-				note: 'This is a note',
-				amount: 2650,
+	it('should edit an expense', () => {
+		const data = {
+			editedExpense: {
+				description: 'New Blah',
+				note: 'Changed note',
+				amount: 666420,
 				createdAt: 1598763926009,
 			},
-		])
+			id: mockData[1].id,
+		}
+
+		const nextState = expenses(mockData, editExpense(data))
+
+		const newMockData = mockData.slice()
+		newMockData[1] = {
+			id: data.id,
+			...data.editedExpense,
+		}
+
+		const rootState = { expenses: nextState, filter: initialFilterState }
+		expect(selectExpenses(rootState)).toEqual(newMockData)
+	})
+
+	it('should not edit expense if expense not found', () => {
+		const data = {
+			editedExpense: {
+				amount: 10,
+			},
+			id: 'invalid id',
+		}
+
+		const nextState = expenses(mockData, editExpense(data))
+
+		const rootState = { expenses: nextState, filter: initialFilterState }
+		expect(selectExpenses(rootState)).toEqual(mockData)
 	})
 })
