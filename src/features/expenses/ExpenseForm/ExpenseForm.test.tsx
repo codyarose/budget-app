d6a6325b152fa507de02c3045f39a1243cb76173
codyarose <crose992@gmail.com>
2020-09-02 22:53:27 -0500

    Refactored tests to use props object and added tests for onSubmit, date change, and focus change

--- src/features/expenses/ExpenseForm/ExpenseForm.test.tsx
+++ src/features/expenses/ExpenseForm/ExpenseForm.test.tsx
@@ -4,6 +4,8 @@ import { shallow } from 'enzyme'
 import ExpenseForm from './index'
 import expenses from '../fixtures'
 import Error from '../../../components/Error'
+import moment from 'moment'
+import { SingleDatePicker } from 'react-dates'
 
 jest.mock('moment', () => {
 	const moment = jest.requireActual('moment')
@@ -15,33 +17,27 @@ jest.mock('moment', () => {
 
 describe('ExpenseForm', () => {
 	it('should render correctly', () => {
-		const wrapper = shallow(
-			<ExpenseForm onSubmit={(expense) => console.log(expense)} />,
-		)
+		const props = { onSubmit: jest.fn() }
+		const wrapper = shallow(<ExpenseForm {...props} />)
 
 		expect(wrapper).toMatchSnapshot()
 	})
 
 	it('should render with expense data', () => {
-		const wrapper = shallow(
-			<ExpenseForm
-				expense={expenses[2]}
-				onSubmit={(expense) => console.log(expense)}
-			/>,
-		)
+		const props = { onSubmit: jest.fn(), expense: expenses[2] }
+		const wrapper = shallow(<ExpenseForm {...props} />)
 
 		expect(wrapper).toMatchSnapshot()
 	})
 
 	it('should render error for invalid form submission', () => {
-		const wrapper = shallow(
-			<ExpenseForm onSubmit={(expense) => console.log(expense)} />,
-		)
+		const props = { onSubmit: jest.fn() }
+		const wrapper = shallow(<ExpenseForm {...props} />)
 		const form = wrapper.find('StyledForm')
-
 		form.simulate('submit', {
 			preventDefault: () => null,
 		})
+
 		expect(wrapper.find(Error).props().helperText).toBe(
 			'Please provide description and amount.',
 		)
@@ -49,21 +45,20 @@ describe('ExpenseForm', () => {
 	})
 
 	it('should set description on input change', () => {
-		const wrapper = shallow(
-			<ExpenseForm onSubmit={(expense) => console.log(expense)} />,
-		)
+		const props = { onSubmit: jest.fn() }
+		const wrapper = shallow(<ExpenseForm {...props} />)
 		const value = 'new description'
-
 		wrapper.find('#expenseDescription').simulate('change', {
 			target: { name: 'description', value },
 		})
+
 		expect(wrapper.find('#expenseDescription').props().value).toEqual(value)
 	})
 
 	it('should set note on input change', () => {
-		const wrapper = shallow(
-			<ExpenseForm onSubmit={(expense) => console.log(expense)} />,
-		)
+		const props = { onSubmit: jest.fn() }
+
+		const wrapper = shallow(<ExpenseForm {...props} />)
 		const value = 'new note'
 
 		wrapper.find('#expenseNote').simulate('change', {
@@ -73,9 +68,8 @@ describe('ExpenseForm', () => {
 	})
 
 	it('should set amount if valid input', () => {
-		const wrapper = shallow(
-			<ExpenseForm onSubmit={(expense) => console.log(expense)} />,
-		)
+		const props = { onSubmit: jest.fn() }
+		const wrapper = shallow(<ExpenseForm {...props} />)
 		const value = '13.69'
 
 		wrapper.find('#expenseAmount').simulate('change', {
@@ -85,14 +79,46 @@ describe('ExpenseForm', () => {
 	})
 
 	it('should not set amount if invalid input', () => {
-		const wrapper = shallow(
-			<ExpenseForm onSubmit={(expense) => console.log(expense)} />,
-		)
+		const props = { onSubmit: jest.fn() }
+		const wrapper = shallow(<ExpenseForm {...props} />)
 		const value = '13.666'
 
 		wrapper.find('#expenseAmount').simulate('change', {
 			target: { value },
 		})
 		expect(wrapper.find('#expenseAmount').props().value).toEqual('')
+	})
+
+	it('should call onSubmit prop for valid form submission', () => {
+		const onSubmitSpy = jest.fn()
+		const props = { onSubmit: onSubmitSpy, expense: expenses[2] }
+		const wrapper = shallow(<ExpenseForm {...props} />)
+		const values = { ...expenses[2] }
+		delete values.id
+
+		wrapper.find('StyledForm').simulate('submit', {
+			preventDefault: () => null,
+		})
+
+		expect(wrapper.find(Error).props().helperText).toBe('')
+		expect(onSubmitSpy).toHaveBeenLastCalledWith(values)
+	})
+
+	it('should set new date on date change', () => {
+		const props = { onSubmit: jest.fn() }
+		const wrapper = shallow(<ExpenseForm {...props} />)
+		const now = moment()
+		wrapper.find(SingleDatePicker).prop('onDateChange')(now)
+
+		expect(wrapper.find(SingleDatePicker).prop('date')).toEqual(now)
+	})
+
+	it('should set calendar focus on change', () => {
+		const props = { onSubmit: jest.fn() }
+		const wrapper = shallow(<ExpenseForm {...props} />)
+		const focused = true
+		wrapper.find(SingleDatePicker).prop('onFocusChange')({ focused })
+
+		expect(wrapper.find(SingleDatePicker).prop('focused')).toEqual(focused)
 	})
 })
