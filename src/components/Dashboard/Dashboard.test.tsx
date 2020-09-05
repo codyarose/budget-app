53d621ce21adcb35726151ef6c60274a9ea8bbba
codyarose <crose992@gmail.com>
2020-09-05 01:28:35 -0500

    Refactored Dashboard to pass selectVisibleExpenses and selectTotal values down to ExpenseTotal and ExpenseList, updated tests

--- src/components/Dashboard/Dashboard.test.tsx
+++ src/components/Dashboard/Dashboard.test.tsx
@@ -1,9 +1,22 @@
 import React from 'react'
 import { shallow } from 'enzyme'
+
 import Dashboard from './index'
+import {
+	selectVisibleExpenses,
+	selectTotal,
+} from '../../features/expenses/expensesSlice'
+import expenses from '../../features/expenses/fixtures'
+
+jest.mock('react-redux', () => ({
+	useSelector: jest.fn((fn) => fn()),
+}))
+jest.mock('../../features/expenses/expensesSlice')
 
 describe('Dashboard component', () => {
 	it('should render correctly', () => {
+		;(selectVisibleExpenses as jest.Mock).mockReturnValue(expenses)
+		;(selectTotal as jest.Mock).mockReturnValue(483248)
 		const wrapper = shallow(<Dashboard />)
 
 		expect(wrapper).toMatchSnapshot()
--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -1,12 +1,23 @@
 import React, { FC } from 'react'
+import { useSelector } from 'react-redux'
+
 import ExpenseList from '../../features/expenses/ExpenseList'
 import ExpenseListFilter from '../../features/filter/ExpenseListFilter'
+import ExpenseTotal from '../../features/expenses/ExpensesTotal'
+import {
+	selectVisibleExpenses,
+	selectTotal,
+} from '../../features/expenses/expensesSlice'
 
 const Dashboard: FC = () => {
+	const expenses = useSelector(selectVisibleExpenses)
+	const total = useSelector(selectTotal)
+
 	return (
 		<div>
+			<ExpenseTotal count={expenses.length} total={total} />
 			<ExpenseListFilter />
-			<ExpenseList />
+			<ExpenseList expenses={expenses} />
 		</div>
 	)
 }
