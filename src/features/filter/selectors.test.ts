99cc3cf14577115a170be6259323e5651cef2db9
codyarose <crose992@gmail.com>
2020-09-05 00:08:23 -0500

    Added missing selector tests

--- src/features/filter/selectors.test.ts
+++ src/features/filter/selectors.test.ts
@@ -1,8 +1,14 @@
 import moment from 'moment'
 
-import { selectFilters } from './filterSlice'
+import {
+	selectFilters,
+	selectText,
+	selectSortBy,
+	selectStartDate,
+	selectEndDate,
+} from './filterSlice'
 
-describe('', () => {
+describe('Selector', () => {
 	const state = {
 		expenses: [],
 		filter: {
@@ -13,22 +19,48 @@ describe('', () => {
 		},
 	}
 
-	it('should return the current filter state', () => {
-		const nextState = selectFilters(state)
+	describe('selectFilters', () => {
+		it('should return the current filter state', () => {
+			const nextState = selectFilters(state)
 
-		expect(nextState).toEqual(state.filter)
+			expect(nextState).toEqual(state.filter)
+		})
+
+		it('should return the current filter state after changes', () => {
+			state.filter = {
+				text: 'different text',
+				sortBy: 'date',
+				startDate: moment(0).startOf('month').add(3, 'days').valueOf(),
+				endDate: moment().endOf('month').subtract(1, 'day').valueOf(),
+			}
+
+			const nextState = selectFilters(state)
+
+			expect(nextState).toEqual(state.filter)
+		})
 	})
 
-	it('should return the current filter state after changes', () => {
-		state.filter = {
-			text: 'different text',
-			sortBy: 'date',
-			startDate: moment(0).startOf('month').add(3, 'days').valueOf(),
-			endDate: moment().endOf('month').subtract(1, 'day').valueOf(),
-		}
+	it('selectText should return text state', () => {
+		const nextState = selectText(state)
 
-		const nextState = selectFilters(state)
+		expect(nextState).toEqual(state.filter.text)
+	})
 
-		expect(nextState).toEqual(state.filter)
+	it('selectSortBy should return sortBy state', () => {
+		const nextState = selectSortBy(state)
+
+		expect(nextState).toEqual(state.filter.sortBy)
+	})
+
+	it('selectStartDate should return startDate state', () => {
+		const nextState = selectStartDate(state)
+
+		expect(nextState).toEqual(state.filter.startDate)
+	})
+
+	it('selectEndDate should return endDate state', () => {
+		const nextState = selectEndDate(state)
+
+		expect(nextState).toEqual(state.filter.endDate)
 	})
 })
