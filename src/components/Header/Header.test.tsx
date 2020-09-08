f9c9dd7f6704c01514adfe8a94d723a0f3c64805
codyarose <crose992@gmail.com>
2020-09-08 00:06:27 -0500

    Added logout button to Header, updated tests

--- src/components/Header/Header.test.tsx
+++ src/components/Header/Header.test.tsx
@@ -1,10 +1,34 @@
 import React from 'react'
-import { shallow } from 'enzyme'
+import { shallow, ShallowWrapper } from 'enzyme'
+import * as redux from 'react-redux'
+
 import Header from './index'
+import { logoutUser } from '../../features/auth/authSlice'
+
+jest.mock('react-redux', () => ({
+	useDispatch: jest.fn(),
+}))
+jest.mock('../../features/auth/authSlice', () => ({
+	logoutUser: jest.fn(),
+}))
+
+const useDispatchSpy = jest.spyOn(redux, 'useDispatch')
+const mockDispatchFn = jest.fn()
+useDispatchSpy.mockReturnValue(mockDispatchFn)
+
+let wrapper: ShallowWrapper
+beforeEach(() => {
+	wrapper = shallow(<Header />)
+})
 
 describe('Header component', () => {
 	it('should render correctly', () => {
-		const wrapper = shallow(<Header />)
 		expect(wrapper).toMatchSnapshot()
+	})
+
+	it('should call logoutUser on button click', () => {
+		wrapper.find('button').simulate('click')
+
+		expect(logoutUser).toHaveBeenCalled()
 	})
 })
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,16 @@
 import React, { FC } from 'react'
 import { NavLink } from 'react-router-dom'
 import styled from 'styled-components'
+import { useDispatch } from 'react-redux'
+
+import { logoutUser } from '../../features/auth/authSlice'
 
 const Header: FC = () => {
+	const dispatch = useDispatch()
+
+	const handleLogout = () => {
+		dispatch(logoutUser())
+	}
 	return (
 		<header>
 			<h1>Budget</h1>
@@ -12,6 +20,7 @@ const Header: FC = () => {
 			<StyledNavLink to="/create" activeClassName="is-active">
 				Create
 			</StyledNavLink>
+			<button onClick={handleLogout}>Logout</button>
 		</header>
 	)
 }
