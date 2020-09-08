38fb83ba0867f7b03eb8c1031cfe9bf714ec853f
codyarose <crose992@gmail.com>
2020-09-08 15:51:04 -0500

    Refactored authSlice actions into async thunks

--- src/features/auth/authSlice.ts
+++ src/features/auth/authSlice.ts
@@ -1,26 +1,55 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
 
 import { firebase, googleAuthProvider } from '../../firebase/firebase'
+import { RootState } from '../../app/store'
 
-const login = () => {
-	firebase.auth().signInWithPopup(googleAuthProvider)
+export const loginUser = createAsyncThunk('auth/loginUser', async () => {
+	try {
+		await firebase.auth().signInWithPopup(googleAuthProvider)
+	} catch (error) {
+		console.log('Error', error)
+	}
+})
+
+export const logoutUser = createAsyncThunk('auth/logoutUser', async () => {
+	try {
+		await firebase.auth().signOut()
+	} catch (error) {
+		console.log('Error', error)
+	}
+})
+
+export interface AuthState {
+	isAuthed: boolean
+	isLoading: boolean
 }
 
-const logout = () => {
-	firebase.auth().signOut()
+export const initialState: AuthState = {
+	isAuthed: false,
+	isLoading: false,
 }
-
-export const initialState = []
 
 export const authSlice = createSlice({
 	name: 'auth',
 	initialState,
-	reducers: {
-		loginUser: login,
-		logoutUser: logout,
+	reducers: {},
+	extraReducers: (builder) => {
+		builder.addCase(loginUser.pending, (state) => {
+			state.isLoading = true
+		})
+		builder.addCase(loginUser.fulfilled, (state) => {
+			state.isAuthed = true
+			state.isLoading = false
+		})
+		builder.addCase(loginUser.rejected, (state) => {
+			state.isLoading = false
+		})
+		builder.addCase(logoutUser.fulfilled, (state) => {
+			state.isAuthed = false
+		})
 	},
 })
 
-export const { loginUser, logoutUser } = authSlice.actions
+export const selectIsAuthed = (state: RootState): boolean => state.auth.isAuthed
 
 export default authSlice.reducer
