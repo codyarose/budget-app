e4a4fc1098b412ce199c2da17e966e583de0a67f
codyarose <crose992@gmail.com>
2020-09-08 00:05:37 -0500

    Hooked up authSlice and added temp check for auth state change

--- src/app/store.ts
+++ src/app/store.ts
@@ -2,11 +2,13 @@ import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit'
 
 import expensesReducer from '../features/expenses/expensesSlice'
 import filterReducer from '../features/filter/filterSlice'
+import authReducer from '../features/auth/authSlice'
 
 export const store = configureStore({
 	reducer: {
 		expenses: expensesReducer,
 		filter: filterReducer,
+		auth: authReducer,
 	},
 })
 
