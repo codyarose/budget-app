ae597cad876adba0b18724c98719bdd164e6392d
codyarose <crose992@gmail.com>
2020-09-09 00:31:17 -0500

    Updated expense fixture with user value

--- src/features/expenses/fixtures.ts
+++ src/features/expenses/fixtures.ts
@@ -5,6 +5,7 @@ export default [
 		note: '',
 		amount: 140500,
 		createdAt: 1598763926001,
+		user: 'user1',
 	},
 	{
 		id: '456def',
@@ -12,6 +13,7 @@ export default [
 		note: 'This is a note',
 		amount: 2650,
 		createdAt: 1598763926009,
+		user: 'user2',
 	},
 	{
 		id: '789ghi',
@@ -19,5 +21,6 @@ export default [
 		note: 'Text goes here',
 		amount: 340098,
 		createdAt: 1598763926345,
+		user: 'user1',
 	},
 ]
