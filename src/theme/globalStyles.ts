1d3c21209dd777f41e4a7b4a170eda18ae7267d7
codyarose <crose992@gmail.com>
2020-09-09 22:40:06 -0500

    Added spacing options and more colors to theme, updated globalStyles

--- src/theme/globalStyles.ts
+++ src/theme/globalStyles.ts
@@ -3,6 +3,10 @@ import { createGlobalStyle } from 'styled-components'
 export const GlobalStyles = createGlobalStyle`
 	html {
 		font-family: system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Ubuntu, "Helvetica Neue", sans-serif;
+		box-sizing: border-box;
+	}
+	*, *::before, *::after {
+		box-sizing: inherit;
 	}
 	body {
 		margin: 0;
--- src/theme/index.ts
+++ src/theme/index.ts
@@ -2,6 +2,15 @@ const theme = {
 	colors: {
 		black: '#000',
 		white: '#fff',
+		gray: '#393e46',
+		accent: '#4ecca3',
+	},
+	spacing: {
+		xs: '0.5rem',
+		sm: '1rem',
+		md: '2rem',
+		lg: '3rem',
+		xl: '4rem',
 	},
 } as const
 
