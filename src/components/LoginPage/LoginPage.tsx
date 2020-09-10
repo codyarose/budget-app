7da58a188e017a55bd9d2c84c68bfbe050115e92
codyarose <crose992@gmail.com>
2020-09-09 22:41:48 -0500

    Added BoxLayout and SignInWithGoogle to LoginPage

--- src/components/LoginPage/LoginPage.tsx
+++ src/components/LoginPage/LoginPage.tsx
@@ -2,6 +2,9 @@ import React, { FC, useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { loginUser, selectIsAuthed } from '../../features/auth/authSlice'
 import { RouteComponentProps } from 'react-router-dom'
+import BoxLayout from '../common/BoxLayout'
+import { SignInWithGoogle } from '../common/SignInWithGoogle'
+import styled from 'styled-components'
 
 type Props = RouteComponentProps
 
@@ -18,11 +21,25 @@ const LoginPage: FC<Props> = ({ history }) => {
 	}, [isAuthed, history])
 
 	return (
-		<div>
-			<h3>Login</h3>
-			<button onClick={handleClick}>Login</button>
-		</div>
+		<BoxLayout.Layout>
+			<BoxLayout.Box>
+				<Title>Expense tracking app</Title>
+				<Subtitle>built with React, Typescript, and Firebase</Subtitle>
+				<p>To try it out:</p>
+				<SignInWithGoogle onClick={handleClick} />
+			</BoxLayout.Box>
+		</BoxLayout.Layout>
 	)
 }
 
 export default LoginPage
+
+const Title = styled.h2`
+	margin: 0 0 ${({ theme }) => theme.spacing.xs};
+	font-size: 2rem;
+`
+const Subtitle = styled.h4`
+	margin: 0 0 ${({ theme }) => theme.spacing.md};
+	font-size: 1rem;
+	font-weight: 500;
+`
