569943370bfcd35ad7d9c23fc4b45b4a6283b299
codyarose <crose992@gmail.com>
2020-09-09 23:36:34 -0500

    Added PageHeader common component

---
+++ src/components/common/PageHeader.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+const PageHeader = styled.div`
+	font-size: 1.5rem;
+	font-weight: 300;
+	& > * {
+		font-size: inherit;
+		margin: 0;
+	}
+`
+
+export default PageHeader
