2a2ce5478710fd4ef03bd36e72769db9f08a592e
codyarose <crose992@gmail.com>
2020-09-10 23:10:16 -0500

    Updated AddExpensePage import order and ContentContainer padding

--- src/components/AddExpensePage/AddExpensePage.tsx
+++ src/components/AddExpensePage/AddExpensePage.tsx
@@ -1,8 +1,8 @@
 import React, { FC } from 'react'
-import ExpenseForm from '../../features/expenses/ExpenseForm'
 import { useDispatch } from 'react-redux'
 import { RouteComponentProps } from 'react-router-dom'
 
+import ExpenseForm from '../../features/expenses/ExpenseForm'
 import { addExpense, ExpenseData } from '../../features/expenses/expensesSlice'
 import ContentContainer from '../common/ContentContainer'
 import PageHeader from '../common/PageHeader'
@@ -18,7 +18,7 @@ const AddExpensePage: FC<Props> = ({ history }) => {
 	}
 
 	return (
-		<ContentContainer>
+		<ContentContainer padBottom={true}>
 			<PageHeader>
 				<h2>Add expense</h2>
 			</PageHeader>
