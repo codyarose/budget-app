ac14d73eb1be263f12692463e052e383a8198da2
codyarose <crose992@gmail.com>
2020-09-10 23:11:41 -0500

    Added setExpenses call to App to populate expenses on fresh log in

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
-import React, { FC } from 'react'
+import React, { FC, useEffect } from 'react'
 import { ThemeProvider } from 'styled-components'
+import { useSelector, useDispatch } from 'react-redux'
 import 'react-dates/lib/css/_datepicker.css'
+import 'react-dates/initialize'
 
 import theme from './theme'
 import { GlobalStyles } from './theme/globalStyles'
 import AppRouter from './routes/AppRouter'
+import { selectUid } from './features/auth/authSlice'
+import { setExpenses, selectExpenses } from './features/expenses/expensesSlice'
 
 const App: FC = () => {
+	const uid = useSelector(selectUid)
+	const expenses = useSelector(selectExpenses)
+	const dispatch = useDispatch()
+
+	useEffect(() => {
+		!!!expenses.length && dispatch(setExpenses(uid))
+	}, [uid, dispatch, expenses.length])
+
 	return (
 		<ThemeProvider theme={theme}>
 			<GlobalStyles />
