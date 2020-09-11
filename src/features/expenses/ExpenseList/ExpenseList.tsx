9007d7f3703c917249db6699bf9b0a2d2ba2a604
codyarose <crose992@gmail.com>
2020-09-10 23:05:37 -0500

    Added list header to ExpenseList and added styles

--- src/features/expenses/ExpenseList/ExpenseList.tsx
+++ src/features/expenses/ExpenseList/ExpenseList.tsx
@@ -1,4 +1,7 @@
 import React, { FC } from 'react'
+import styled from 'styled-components'
+import { rgba } from 'polished'
+
 import ExpenseListItem from '../ExpenseListItem'
 import { Expense } from '../expensesSlice'
 
@@ -8,16 +11,68 @@ interface Props {
 
 const ExpenseList: FC<Props> = ({ expenses }) => {
 	return (
-		<div>
+		<StyledWrapper>
 			{expenses.length === 0 ? (
-				<p>Add an expense</p>
+				<StyledNoExpenses>
+					Use the{' '}
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 24 24"
+						stroke="currentColor"
+						width="25"
+					>
+						<path
+							strokeLinecap="round"
+							strokeLinejoin="round"
+							strokeWidth={2}
+							d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+						/>
+					</svg>{' '}
+					below to add an expense
+				</StyledNoExpenses>
 			) : (
-				expenses.map((expense) => (
-					<ExpenseListItem key={expense.id} {...expense} />
-				))
+				<>
+					<StyledListHeader>
+						<div className="show-for-mobile">Expenses</div>
+						<div className="hide-for-mobile">Expense</div>
+						<div className="hide-for-mobile">Amount</div>
+					</StyledListHeader>
+					<StyledExpenseList>
+						{expenses.map((expense) => (
+							<ExpenseListItem key={expense.id} {...expense} />
+						))}
+					</StyledExpenseList>
+				</>
 			)}
-		</div>
+		</StyledWrapper>
 	)
 }
 
 export default ExpenseList
+
+const StyledWrapper = styled.div`
+	padding-top: ${({ theme }) => theme.spacing.sm};
+`
+
+const StyledListHeader = styled.div`
+	display: flex;
+	justify-content: space-between;
+	padding: ${({ theme }) => `${theme.spacing.sm} ${theme.spacing.sm}`};
+	color: ${({ theme }) => rgba(theme.colors.black, 0.5)};
+	border-bottom: 1px solid currentColor;
+`
+
+const StyledExpenseList = styled.ul`
+	list-style-type: none;
+	padding: 0;
+	margin: 0;
+	& > *:not(:last-of-type) {
+		border-bottom: 1px solid ${({ theme }) => rgba(theme.colors.black, 0.5)};
+	}
+`
+
+const StyledNoExpenses = styled.div`
+	text-align: center;
+	font-size: 1.5rem;
+`
