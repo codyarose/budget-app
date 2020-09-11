b8109c72af7b1d958fa2baf417dff257185cb1de
codyarose <crose992@gmail.com>
2020-09-10 23:07:08 -0500

    Refactored loginUser to ensure that user details are set before setExpenses is called

--- src/features/auth/authSlice.ts
+++ src/features/auth/authSlice.ts
@@ -6,10 +6,13 @@ import { RootState } from '../../app/store'
 export const loginUser = createAsyncThunk('auth/loginUser', async () => {
 	try {
 		const login = await firebase.auth().signInWithPopup(googleAuthProvider)
-		const { uid, displayName } = login.user!
-		const authUser = { uid, displayName }
-		localStorage.setItem('authUser', JSON.stringify(authUser))
-		setUser(authUser)
+		const user = login.user
+		if (user) {
+			const { uid, displayName } = user
+			const authUser = { uid, displayName }
+			localStorage.setItem('authUser', JSON.stringify(authUser))
+			return authUser
+		}
 	} catch (error) {
 		console.log(error)
 	}
@@ -26,7 +29,7 @@ export const logoutUser = createAsyncThunk('auth/logoutUser', async () => {
 
 interface User {
 	uid: string
-	displayName: string
+	displayName: string | null
 }
 
 export interface AuthState {
@@ -57,9 +60,14 @@ export const authSlice = createSlice({
 		builder.addCase(loginUser.pending, (state) => {
 			state.isLoading = true
 		})
-		builder.addCase(loginUser.fulfilled, (state) => {
+		builder.addCase(loginUser.fulfilled, (state, action) => {
 			state.isAuthed = true
 			state.isLoading = false
+			state.user = {
+				...state.user,
+				uid: action.payload ? action.payload.uid : '',
+				displayName: action.payload ? action.payload.displayName : '',
+			}
 		})
 		builder.addCase(loginUser.rejected, (state) => {
 			state.isLoading = false
