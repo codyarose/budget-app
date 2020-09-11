98e91298077017807b30df744bfcfe2bbf4f5c56
codyarose <crose992@gmail.com>
2020-09-10 23:10:36 -0500

    Added padBottom option to ContentContainer

--- src/components/common/ContentContainer.ts
+++ src/components/common/ContentContainer.ts
@@ -1,10 +1,12 @@
 import styled from 'styled-components'
 
-const ContentContainer = styled.div`
+const ContentContainer = styled.div<{ padBottom?: boolean }>`
 	width: 100%;
 	max-width: 64rem;
 	margin: 0 auto;
 	padding: ${({ theme }) => `0 ${theme.spacing.md}`};
+	${(props) =>
+		props.padBottom && `padding-bottom: ${props.theme.spacing.lg};`}
 `
 
 export default ContentContainer
